9742d36613ad397856a2ef712bc4e8edb6cf3cbe
mjkdev2024 <mjkdev2024@gmail.com>
2025-08-26 02:27:47 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+response = client.campaigns.list() # list campaigns
+response = client.campaigns.get("campaign_id") # get campaign info
+response = client.ecommerce.get_all_store_customers("store_id") # ecom customers
+response = client.ecommerce.orders() # ecom orders
+response = client.ecommerce.get_all_order_line_items("store_id", "order_id") # ecom order line items
+
+# TODO: Investigate Lists/Audiences section of API Docs
